3019d3b5e277254c7c84f5fef01a2fd3f82fa701
94-c <hyeongwoo26@gmail.com>
2023-05-23 16:23:25 +0900

    # 카테고리 레퍼지토리 쿼리 실행 부분 삭제

--- src/main/java/com/blog/api/repository/CategoryRepository.java
+++ src/main/java/com/blog/api/repository/CategoryRepository.java
@@ -9,7 +9,5 @@ import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query("SELECT c FROM Category c LEFT JOIN c.parent p ORDER BY p.id ASC NULLS FIRST, c.id ASC")
-    List<Category> findAllOrderByParentIdAscNullsFirstCategoryIdAsc();
 
 }
